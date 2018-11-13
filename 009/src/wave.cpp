00bb54b36da013f8b949f7a761654c90138cf778
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2018-11-13 15:22:21 +0100

    waver one

--- 009/src/wave.cpp
+++ 009/src/wave.cpp
@@ -14,7 +14,7 @@ void Wave::setup(){
 //--------------------------------------------------------------
 void Wave::update(int rows){
   rowNumb = rows;
-  rowH = ofGetHeight() / rows;
+  rowH = (ofGetHeight() + 20) / rows;
   for (unsigned int i = 0; i < rows; i++) {
     WaveVertex vert;
     vert.topLeft = ofVec2f(0, rowH * i);
@@ -23,32 +23,38 @@ void Wave::update(int rows){
     vert.bottomRight = ofVec2f(ofGetWidth() + 1, rowH * (i + 1));
     verts.push_back(vert);
   }
+  
+  
 }
 
 //--------------------------------------------------------------
 void Wave::draw(int points){
-  ofSetColor(200, 200, 100);
-  ofNoFill();
+  float waver = ofGetFrameNum() * 0.005;
+  
+  
+  
   for (unsigned int i = 0; i < rowNumb; i++) {
+    
+    ofSetColor(200, 200, 100, 20 * i);
+
     ofBeginShape();
     ofVertex(verts[i].topLeft);
-    
-    for (unsigned int j = 0; j < points; j++) {
+    for (unsigned int j = 0; j < points + 1; j++) {
       int x = (ofGetWidth() / points) * j;
-      int y = verts[i].topLeft.y;
-      ofVertex(x, y);
+      float y = verts[i].topLeft.y + (sin((j + waver) * TWO_PI * 2 / points) * 10);
+//      ofVertex(x, y);
+      ofCurveVertex(x, y);
     }
-    
     ofVertex(verts[i].topRight);
     ofVertex(verts[i].bottomRight);
-    
-    for (unsigned int j = 0; j < points; j++) {
-      int x = ((ofGetWidth() / points) * j) - ofGetWidth();
-      int y = verts[i].bottomRight.y;
-      ofVertex(x, y);
+    for (unsigned int j = points; j + 1 > 0; j--) {
+      int x = (ofGetWidth() / points) * j;
+      float y = verts[i].bottomRight.y + (sin((j + waver) * TWO_PI * 2 / points) * 10);
+//      ofVertex(x, y);
+      ofCurveVertex(x, y);
     }
-    
     ofVertex(verts[i].bottomLeft);
-    ofEndShape();
+    ofEndShape(true);
+    
   }
 }
